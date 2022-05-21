efa82798fdcb678dff26e4aa4bf499039a6508ef
Aine <84486763+merstella@users.noreply.github.com>
2022-05-21 13:15:03 +0700

    better implementation for LCA

--- LCA-binary-lifting.cpp
+++ LCA-binary-lifting.cpp
@@ -1,89 +1,100 @@
-// Problem link : https://www.spoj.com/problems/LCA/
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
-vector<vector<int>> adj; // 0-based index
-vector<bool> visited;
-vector<int> height, parent;
-vector<vector<int>> P; // store all 2 ^ j ancestors of node i
-void init(int n) {
-	const int LOGN = (int)ceil(log2(n));
-	adj.resize(n);
-	visited.assign(n, false);
-	height.resize(n);
-	parent.assign(n, -1);
-	P.assign(n, vector<int> (LOGN, -1));
+
+#ifdef ON_PC
+    #include "debug.h"
+#else 
+    #define deb(x...) 12
+#endif
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+template<class T> using MaxHeap = priority_queue<T, vector<T>,    less<T>>;
+template<class T> using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename T1> T amax(T &a, T1 b) {if (b > a) a = b; return a;}
+template<typename T, typename T1> T amin(T &a, T1 b) {if (b < a) a = b; return a;}
+
+#define fi first
+#define se second
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+
+const int maxN = 1e3 + 2;
+const int MOD[2] = {1000000007, 998244353};
+const int inf = INT_MAX;
+const ll linf = LLONG_MAX;
+const int LOG = 20;
+
+vector<int> g[maxN];
+int num[maxN], tail[maxN];
+int timeDFS = 0;
+vector<vector<int>> up(maxN, vector<int>(LOG));
+void dfs(int u, int p) {
+    num[u] = ++timeDFS;
+    up[u][0] = p;
+    for (int i = 1; i < LOG; ++i) {
+        up[u][i] = up[up[u][i - 1]][i - 1];
+    }
+    for (auto v : g[u]) {
+        if (v == p) continue;
+        dfs(v, u);
+    }
+    tail[u] = timeDFS;
 }
-void clr(int n) {
-	visited.clear(); height.clear(); parent.clear();
-	for (int i = 0; i < n; ++i) {
-		adj[i].clear();
-		P[i].clear();
-	}
-}
-void dfs(int node, int h = 0) {
-	visited[node] = true;
-	height[node] = h;
-	for (auto to : adj[node]) {
-		if (!visited[to]) {
-			parent[to] = node;
-			dfs(to, h + 1);
-		}
-	}
-}
-void buildLCA(int n) {
-	for (int i = 0; i < n; ++i) {
-		P[i][0] = parent[i];
-	}
-	for (int j = 1; (1 << j) < n; ++j) {
-		for (int i = 0; i < n; ++i) {
-			if (P[i][j - 1] != -1) {
-				P[i][j] = P[P[i][j - 1]][j - 1];
-			}
-		}
-	}
+bool isAncestor(int u, int v){
+    return (num[u] <= num[v] && num[v] <= tail[u]);
 }
 int findLCA(int u, int v) {
-	if (height[v] > height[u]) swap(u, v);
-	const int LOGH = (int)ceil(log2(height[u]));
-	for (int i = LOGH; i >= 0; --i) {
-		if (height[u] - (1 << i) >= height[v]) {
-			u = P[u][i];
-		}
-	}
-	if (u == v) return u;
-	for (int i = LOGH; i >= 0; --i) {
-		if (P[u][i] != -1 && P[u][i] != P[v][i]) {
-			u = P[u][i];
-			v = P[v][i];
-		}
-	}
-	return parent[u];
+    if (isAncestor(u, v)) return u;
+    if (isAncestor(v, u)) return v;
+    for (int i = LOG - 1; i >= 0; --i) {
+        if (!isAncestor(up[u][i], v)) {
+            u = up[u][i];
+        }
+    }
+    return up[u][0];
 }
-int main() {
-	cin.tie(0)->sync_with_stdio(false);
-	int tc;
-	cin >> tc;
-	for (int t = 1; t <= tc; ++t) {
-		int numNode; cin >> numNode;
-		init(numNode);
-		for (int i = 0; i < numNode; ++i) {
-			int numChild; cin >> numChild;
-			while (numChild--) {
-				int to; cin >> to; --to;
-				adj[i].push_back(to);
-				adj[to].push_back(i);
-			}
-		}
-		// DFS to find height and parent of each node
-		dfs(0);
-		buildLCA(numNode);
-		cout << "Case " << t << ":\n";
-		int numQ; cin >> numQ;
-		while (numQ--) {
-			int u, v; cin >> u >> v; --u; --v;
-			cout << findLCA(u, v) + 1 << '\n';
-		}
-		clr(numNode);
-	}
-	return 0;
+void reset() {
+    for (int i = 0; i < maxN; ++i) {
+        g[i].clear();
+    }
+    timeDFS = 0;
 }
+void solve() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        int m;
+        cin >> m;
+        while (m--) {
+            int u;
+            cin >> u;
+            g[i].push_back(u);
+            g[u].push_back(i);
+        }
+    }
+    dfs(1, 1);
+    int q; cin >> q;
+    while (q--) {
+        int u, v;
+        cin >> u >> v;
+        cout << findLCA(u, v) << '\n';
+    }
+    reset();
+}
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(false);
+    
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        cout << "Case " << i << ":\n";
+        solve();
+    }
+
+    cout << '\n';
+    return 0;
+}
+// -~.-~-.-~-| Eustia Tia Astraea |-~-.-~-.~-
